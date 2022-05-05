774fa34e430c074d2bc45ac071285d8ae6639d32
radik <kamailoff@gmail.com>
2022-05-05 23:15:53 +0300

    feat: add Factories

---
+++ database/factories/Point/PointFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories\Point;
+
+use App\Models\Point\PointType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PointFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $point_types = PointType::getTypes();
+        return [
+            'name' => $this->faker->city,
+            'type' => $point_types[$this->faker->numberBetween(0, max($point_types))],
+            'code' => $this->faker->countryCode,
+            'address' => $this->faker->address,
+            'latitude' => $this->faker->latitude,
+            'longitude' => $this->faker->longitude,
+        ];
+    }
+}

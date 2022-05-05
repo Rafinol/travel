1e5ef38c93205b59f9132dd15159f17d6f532a55
radik <kamailoff@gmail.com>
2022-05-05 23:15:08 +0300

    feat: add city migration, add status column, add transport type ...

---
+++ database/migrations/2022_05_05_175137_create_cities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\City\City;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('points', function (Blueprint $table) {
+            $table->foreignId('city_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('points', function (Blueprint $table) {
+            $table->dropForeignIdFor(City::class);
+            $table->dropColumn('city_id');
+        });
+        Schema::dropIfExists('cities');
+    }
+};
